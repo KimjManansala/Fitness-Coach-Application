931dac951f7874aa02d8d2fbe594fcb777957501
KimJ <kimjmanansala@northspyre.com>
2021-05-15 17:54:27 -0400

    Removed any passport items. will deal with that later. Focus on building core functionality with mock function

--- Server/Server.js
+++ Server/Server.js
@@ -4,33 +4,6 @@ const compress = require('compression');
 const path = require('path');
 const bodyParser = require('body-parser');
 require('dotenv').config()
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-
-
-// OAuth 2.0-based strategies require a `verify` function which receives the
-// credential (`accessToken`) for accessing the Facebook API on the user's
-// behalf, along with the user's profile.  The function must invoke `cb`
-// with a user object, which will be set at `req.user` in route handlers after
-// authentication.
-passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_AUTH_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_AUTH_CLIENT_SECRET,
-    callbackURL: '/auth/google/callback"'
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    // In this example, the user's Facebook profile is supplied as the user
-    // record.  In a production-quality application, the Facebook profile should
-    // be associated with a user record in the application's database, which
-    // allows for account linking and authentication with other identity
-    // providers.
-    // function(accessToken, refreshToken, profile, cb) {
-    //     User.findOrCreate({ googleId: profile.id }, function (err, user) {
-    //       return cb(err, user);
-    //     });
-    //   }
-    return cb(null, profile);
-  }));
 
 
 // heroku
@@ -51,24 +24,9 @@ app.use(bodyParser.json({limit: '5mb'}));
 app.use(bodyParser.urlencoded({ limit: '5mb', extended: false }));
 
 
-// ALL ROUTES WILL GO HERE IMPORTED FROM THE ROUTES IN THE API DIRECTORY
-app.get('/auth/google',
-  function (req, res) {
-    passport.authenticate('google', { scope: ['profile'] })
-  }
-  );
-
-app.get('/auth/google/callback', 
-  passport.authenticate('google', { failureRedirect: '/login' }),
-  function(req, res) {
-      console.log('helloo')
-    // Successful authentication, redirect home.
-    res.redirect('/');
-  });
-
 // frontend entry
 app.use('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'build/index.html'));
+    res.sendFile(path.join(__dirname, '..', 'Build/index.html'));
 });
 
 
