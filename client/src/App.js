73f832b05d6e67862fbf0c8a51cbf302f7af4cf1
KimJ <kimjmanansala@northspyre.com>
2021-05-16 15:44:46 -0400

    skeleton created. going to start with the table

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Navbar from './features/Navbar/Navbar.jsx';
-import CoreMetrics from './features/CoreMetrics/CoreMetrics.jsx';
-import { REACT_URLS } from './app/api_and_urls.js';
+import Navbar from "./features/Navbar/Navbar.jsx";
+import CoreMetrics from "./features/CoreMetrics/CoreMetrics.jsx";
+import { REACT_URLS } from "./app/api_and_urls.js";
+import { Container } from "semantic-ui-react";
 
 function App() {
-  return (
-    <Router>
-          <Navbar />
-            <Switch>
-                {/* TODO MAKE PRIVATE ROUTES */}
-                {/* TODO MAKE UNKNOWN PAGE AND UNAUTHROIZED PAGE */}
-                <Route path={REACT_URLS.CORE_METICS} component={CoreMetrics} />
-                <Route path={REACT_URLS.CORE_METICS_1} component={CoreMetrics} />
-            </Switch>
-    </Router>
-  );
+    return (
+        <Router>
+            <Navbar />
+            <div
+                style={{
+                    margin: "5px",
+                }}
+            >
+                <Container fluid>
+                    <Switch>
+                        {/* TODO MAKE PRIVATE ROUTES */}
+                        {/* TODO MAKE UNKNOWN PAGE AND UNAUTHROIZED PAGE */}
+                        <Route
+                            path={REACT_URLS.CORE_METICS}
+                            component={CoreMetrics}
+                        />
+                        <Route
+                            path={REACT_URLS.CORE_METICS_1}
+                            component={CoreMetrics}
+                        />
+                    </Switch>
+                </Container>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
--- client/src/features/CoreMetrics/CoreMetrics.jsx
+++ client/src/features/CoreMetrics/CoreMetrics.jsx
@@ -1,14 +1,22 @@
 import React, { useEffect } from 'react'
 import PropTypes from 'prop-types'
+import { Card } from 'semantic-ui-react'
+import CoreMetricsTable from './CoreMetricsTable'
 
 const CoreMetrics = props => {
     useEffect(() => {
         console.log('hello how are you')
     })
     return (
-        <div>
-            Hello I will be the core metrics
-        </div>
+        <React.Fragment>
+            <Card>
+                <Card.Content header='Place holder for chart' />
+                <Card.Content description='Place holder for chart' />
+                <Card.Content extra>
+                </Card.Content>
+            </Card>
+            <CoreMetricsTable />
+            </React.Fragment>
     )
 }
 
