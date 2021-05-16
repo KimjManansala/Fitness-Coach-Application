e7b3b408a24465cb133975ded2be5d4f42df53d3
KimJ <kimjmanansala@northspyre.com>
2021-05-15 20:47:44 -0400

    Development is going well. Going to need to start core work

---
+++ client/src/app/api_and_urls.js
@@ -0,0 +1,9 @@
+export const API_ENDPOINTS = {
+    user_information: 'user_information'
+};
+
+export const REACT_URLS = {
+    CORE_METICS: "/core_metrics",
+    CORE_METICS_1: "/core_metrics/:id"
+
+};
